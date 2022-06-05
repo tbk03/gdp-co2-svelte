9cc2532b0cf724fa56361c1825ce6af324c21509
tbk03 <chrisjmartin03@gmail.com>
2022-06-05 21:25:44 +0100

    working out how to read in a local csv

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,12 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import Standardplot from "./components/Standardplot.svelte";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
+
   <h1>Hello world!</h1>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Standardplot/>
 </main>
 
 <style>
@@ -32,10 +21,10 @@
     margin: 0 auto;
   }
 
-  img {
+  /* img {
     height: 16rem;
     width: 16rem;
-  }
+  } */
 
   h1 {
     color: #ff3e00;
@@ -47,19 +36,19 @@
     max-width: 14rem;
   }
 
-  p {
+  /* p {
     max-width: 14rem;
     margin: 1rem auto;
     line-height: 1.35;
-  }
+  } */
 
   @media (min-width: 480px) {
     h1 {
       max-width: none;
     }
 
-    p {
+    /* p {
       max-width: none;
-    }
+    } */
   }
 </style>
---
+++ src/components/Standardplot.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { csv } from "d3-fetch";
+
+    // let dataset;
+    // csv("https://github.com/tbk03/gdp-co2-R/blob/main/data_out/gdp_co2_2015.csv")
+    //     .then((d) => dataset = d);  
+    
+    let data;
+    csv("./data/gdp_co2_2015.csv")
+        .then((d) => data = d);
+    $: console.log(data);
+</script>
